0697da609addd47f02d176763ff373812e3286b9
Marat <maratos2001@mail.ru>
2023-04-17 14:37:40 +0300

    set port for the server and create route for html page

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require('express')
 const calculator = express()
 const port = 3000
+
+calculator.listen(port)
+
+calculator.get("/", function (req, res) {
+    res.sendFile(`${__dirname}/index.html`)
+})
