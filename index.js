9fcc851f8c81c0d89e4e02d3f7a5167389d3dcf5
Marat <maratos2001@mail.ru>
2023-04-17 15:43:50 +0300

    add routes to send info about weight and height and to get bmi

--- index.js
+++ index.js
@@ -14,3 +14,14 @@ calculator.post("/", function (req, res) {
     let result = Number(req.body.num1) + Number(req.body.num2)
     res.send(`The result is ${result}`)
 })
+
+calculator.get("/bmicalculator", function (req, res) {
+    res.sendFile(`${__dirname}/bmiCalculator.html`)
+})
+
+calculator.post("/bmicalculator", function (req, res) {
+    let weight = Number(req.body.weight)
+    let height = Number(req.body.height)
+    let bmi = weight / height ** 2
+    res.send(`Your BMI is ${bmi}`)
+})
