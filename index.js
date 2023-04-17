a214a895f71e56fe26cf238b4adf83f4a2fb110b
Marat <maratos2001@mail.ru>
2023-04-17 15:32:22 +0300

    add post method to sum two numbers and give a response

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const calculator = express()
 const port = 3000
+calculator.use(bodyParser.urlencoded({extended: true}))
 
 calculator.listen(port)
 
 calculator.get("/", function (req, res) {
     res.sendFile(`${__dirname}/index.html`)
+})
+
+calculator.post("/", function (req, res) {
+    let result = Number(req.body.num1) + Number(req.body.num2)
+    res.send(`The result is ${result}`)
 })
